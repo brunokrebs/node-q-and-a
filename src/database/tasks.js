0055f7f1b7662da0fbe81d9914df9da5236719e6
Bruno Krebs <krebs.bruno@gmail.com>
2022-05-02 08:59:46 -0300

    first version

---
+++ src/database/tasks.js
@@ -0,0 +1,44 @@
+const { ObjectId } = require("mongodb");
+const { getDatabase } = require("./mongo");
+
+const collectionName = "tasks";
+
+async function insertTask(task) {
+  const database = await getDatabase();
+  const { insertedId } = await database
+    .collection(collectionName)
+    .insertOne(task);
+  return insertedId;
+}
+
+async function getTasks() {
+  const database = await getDatabase();
+  return await database.collection(collectionName).find({}).toArray();
+}
+
+async function deleteTask(id) {
+  const database = await getDatabase();
+  await database.collection(collectionName).deleteOne({
+    _id: new ObjectId(id),
+  });
+}
+
+async function updateTask(id, task) {
+  const database = await getDatabase();
+  delete task._id;
+  await database.collection(collectionName).update(
+    { _id: new ObjectID(id) },
+    {
+      $set: {
+        ...task,
+      },
+    }
+  );
+}
+
+module.exports = {
+  insertTask,
+  getTasks,
+  deleteTask,
+  updateTask,
+};
---
+++ src/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const { startDatabase } = require("./database/mongo");
+const {
+  deleteTask,
+  insertTask,
+  getTasks,
+  updateTask,
+} = require("./database/tasks");
+
+// defining the Express app
+const app = express();
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan("combined"));
+
+// defining an endpoint to return all tasks
+app.get("/", async (req, res) => {
+  res.send(await getTasks());
+});
+
+app.post("/", async (req, res) => {
+  const newTask = req.body;
+  await insertTask(newTask);
+  res.send({ message: "New task inserted." });
+});
+
+// endpoint to delete a task
+app.delete("/:id", async (req, res) => {
+  await deleteTask(req.params.id);
+  res.send({ message: "Task removed." });
+});
+
+// endpoint to update a task
+app.put("/:id", async (req, res) => {
+  const updatedTask = req.body;
+  await updateTask(req.params.id, updatedTask);
+  res.send({ message: "Task updated." });
+});
+
+// starting the server
+startDatabase().then(async () => {
+  await insertTask({ title: "Buy milk" });
+
+  // start the server
+  app.listen(3001, async () => {
+    console.log("listening on port 3001");
+  });
+});
